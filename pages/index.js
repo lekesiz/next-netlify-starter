54c0828a55917169f31233dcb3f18e15367d9d1b
lekesiz <mikail@lekesiz.org>
2023-03-02 13:27:29 +0100

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Mikail Lekesiz" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          mikail@lekesiz.org
         </p>
       </main>
 
